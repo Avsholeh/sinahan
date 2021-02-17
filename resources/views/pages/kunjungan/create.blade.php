d9dfec1ab9522d8517f4d06e8c17eb73d0500377
avsholeh <avsholeh@gmail.com>
2021-02-18 00:29:04 +0700

    fix

--- resources/views/pages/kunjungan/create.blade.php
+++ resources/views/pages/kunjungan/create.blade.php
@@ -8,30 +8,33 @@
 
     <div class="row">
         <div class="col-lg-6 col-xl-6">
-
+            <!-- KUNJUNGAN -->
             <div class="card shadow mb-4">
                 <div class="card-body">
                     <form>
                         <div class="form-group">
                             <label for="narapidana">Narapidana</label>
-                            <input name="narapidana" type="text" class="form-control" id="narapidana" aria-describedby="narapidana_help">
+                            <input name="narapidana" type="text" class="form-control" id="narapidana"
+                                   aria-describedby="narapidana_help">
                         </div>
 
                         <div class="form-group">
                             <label for="berlaku">Berlaku Hingga</label>
-                            <input name="berlaku" type="date" class="form-control" id="berlaku" aria-describedby="berlaku" value="2021-02-01" disabled>
+                            <input name="berlaku" type="date" class="form-control" id="berlaku"
+                                   aria-describedby="berlaku" value="" disabled>
                         </div>
 
                         <div class="form-group">
                             <label for="keperluan">Keperluan</label>
-                            <textarea class="form-control" name="keperluan" id="keperluan" cols="30" rows="3" aria-describedby="keperluan"></textarea>
+                            <textarea name="keperluan" class="form-control" id="keperluan" cols="30" rows="3"
+                                      aria-describedby="keperluan"></textarea>
                         </div>
 
                         <button type="submit" class="btn btn-primary">Simpan</button>
                     </form>
                 </div>
             </div>
-
+            <!-- END KUNJUNGAN -->
         </div>
     </div>
 
