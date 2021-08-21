4a46b3fac347776e1c087299419caa5fe2fecc30
avsholeh <avsholeh@gmail.com>
2021-08-21 14:58:38 +0700

    local push

--- app/Http/Controllers/JaksaController.php
+++ app/Http/Controllers/JaksaController.php
@@ -52,7 +52,7 @@ class JaksaController extends Controller
     public function defaultFoto(Request $request)
     {
         if (!$request->foto) {
-            if ($request->jenis_kelamin === 'Perempuan') {
+            if (strtolower($request->jenis_kelamin) === 'wanita') {
                 return base64_encode(File::get(storage_path('app/public/perempuan.png')));
             } else {
                 return base64_encode(File::get(storage_path('app/public/laki.png')));
